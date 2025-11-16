e7d40a4838566cd269e73b6230da55dc06882a94
Ivan Krushelnytskyi <vanffnan15@gmail.com>
2025-11-16 16:50:28 +0200

    2

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000", // Rails API
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   headers: {
     "Content-Type": "application/json"
   }
